7df2eecc9249d2ef1c2f2480b783e63102033e37
Pavel <pavelbog@mail.ru>
2017-04-15 03:44:31 +0500

    15.04

--- js/pagescript.js
+++ js/pagescript.js
@@ -18,3 +18,32 @@ $(document).ready(function() {
     })
     console.log('ready');
 });
+
+$(document).ready(function(){
+  $('.sli, .multiple-items').slick({
+    infinite: true,
+    slidesToShow: 3,
+    dots: true,
+    //количество слайдов, которые выводятся на экране
+    slidesToScroll: 1
+    //количество слайдов, которые перелистываются за один раз
+  });
+  $('.single-item').slick({
+      accessibility: false,
+      arrows: false
+  });
+  $('.slider-menu div.td').eq(0).addClass('active');
+  $('.slider-menu div.td').on('click', function() {
+      var index = $(this).index();
+      var slide = $(this).parent().parent().parent().find('.slider .slick-slide').eq(index);
+      $('.single-item').slick('slickGoTo', index);
+  });
+  $('.single-item').on('beforeChange', function(event, slick, currentSlide, nextSlide){
+      $('.slider-menu div.td').removeClass('active');
+      $('.slider-menu div.td').eq(nextSlide).addClass('active');
+  });
+  $('.single-item').on('init', function(event, slick){
+      console.log('123');
+      console.log($(slick).slick('slickCurrentSlide'));
+  });
+});
